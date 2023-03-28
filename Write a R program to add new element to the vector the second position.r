018b7512a68c70924ab9d0866f4d0fecbad20797
JAIDON GILL SHAJAN <84973988+JUDU-13@users.noreply.github.com>
2023-03-29 00:33:07 +0530

    add

--- Write a R program to add new element to the vector the second position.r
+++ Write a R program to add new element to the vector the second position.r
@@ -9,3 +9,16 @@ my_vector <- c(my_vector[1], 23, my_vector[-1])
 print(my_vector)
 
 
+#OR
+
+# Define the vector
+my_vector <- c(24, 56, 67)
+
+# Use a for loop to add the element "23" to the second position
+for (i in (length(my_vector)+1):3) {
+  my_vector[i] = my_vector[i-1]
+}
+my_vector[2] <- 23
+
+# Print the modified vector
+print(my_vector)
